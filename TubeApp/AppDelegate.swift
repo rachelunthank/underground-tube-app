702c6fd6b3b9202e6703f735052b5ddcd5240f05
Rachel Unthank <rachelunthank@gmail.com>
2019-02-21 23:07:54 +0000

    Remove storyboard and other tabs, replace status view with nibs

--- TubeApp/AppDelegate.swift
+++ TubeApp/AppDelegate.swift
@@ -13,7 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        registerForPushNotifications()
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        let statusViewController = LineStatusViewController()
+        let navController = UINavigationController(rootViewController: statusViewController)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
         return true
     }
 
